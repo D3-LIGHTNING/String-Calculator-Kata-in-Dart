d8e6362ef509e1f43d6e7822e0a275dd738ecfd5
Pradhu <vidhuramachandran1@gmail.com>
2025-08-28 20:43:18 +0530

    Step2:  allow add method to handle any amount of numbers.
    Allow the add method to handle any amount of numbers. Test successfully passed. No change on the solution was required to pass this step.

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -2,7 +2,7 @@ import 'package:string_calculator/string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('String Calculator', () {
+  group('String Calculator step 1', () {
     test('returns 0 when input is empty string', () {
       expect(add(''), equals(0));
     });
@@ -15,6 +15,13 @@ void main() {
     test('returns the sum when input has two numbers separated by comma', () {
       expect(add('1,2'), equals(3));
       expect(add('10,20'), equals(30));
+    });
+  });
+
+  group('String Calculator step 2', () {
+    test("returns the sume of multiple numbers separated by commas", () {
+      expect(add('1,2,3,4,5'), equals(15));
+      expect(add('10,20,30,40'), equals(100));
     });
   });
 }
